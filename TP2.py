36fe9e6d72da85cab3ea937348f13c6c27255fe7
Santibianucci <87279083+Santibianucci@users.noreply.github.com>
2021-07-21 20:39:37 -0300

    arregle

--- TP2.py
+++ TP2.py
@@ -115,27 +115,31 @@ def crear_carpetas():
     docentes = []
     alumnos = []
     evaluacion = "AlgebraII"
-    os.mkdir(evaluacion)
-    i = 0
 
-    # carpeta de los docentes
-    with open(r"C:\COSAS MUY IMPORTANTES\Facultad\python\docentes.csv", "r") as csv_file:
-        for csv_file in csv_file.readlines():
-            nombresDocentes = csv_file.split(',')
-            os.mkdir((os.path.join(evaluacion, nombresDocentes[0])))
+    BASEDIR = os.path.dirname(os.path.abspath(__file__))
+
+    print(BASEDIR)
+    RUTA = os.path.join(BASEDIR, evaluacion)
+    os.mkdir(RUTA)
+    print(RUTA)
+    RUTADOC = os.path.join(BASEDIR, 'docentes.csv')
+    with open(RUTADOC, "r") as csv_file:
+        for linea in csv_file.readlines():
+            linea = linea.rstrip()
+            nombresDocentes = linea.split(',')
+            os.mkdir((os.path.join(RUTA, nombresDocentes[0])))
             docentes.append(nombresDocentes)
     with open(r"C:\COSAS MUY IMPORTANTES\Facultad\python\docalum.csv", "r") as csv_file1:
-        for csv_file1 in csv_file1.readlines():
-            list_alumdoc = csv_file1.split(',')
+        for linea2 in csv_file1.readlines():
+            linea2 = linea2.rstrip()
+            list_alumdoc = linea2.split(',')
             alumnos.append(list_alumdoc)
         print(alumnos)
-        for i in range(len(docentes)):
-            for k in range(len(alumnos)):
-                print(evaluacion, docentes[i][0], alumnos[k][1])
-                if alumnos[k][0] == docentes[i][0]:
-                    os.mkdir((os.path.join(evaluacion, docentes[i][0], alumnos[k][1])))
-                print(os.path.join(evaluacion, docentes[i][0], alumnos[k][1]))
-=======
+        for docente in docentes:
+            for alumno in alumnos:
+                print(evaluacion, docente[0], alumno[1])
+                if alumno[0] == docente[0]:
+                    os.mkdir((os.path.join(RUTA, docente[0], alumno[1])))
 
 
 main()
