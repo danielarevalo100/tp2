3172a848f4b07b87bf52dee7eb86d1d5f4467955
MateoLardiez <mlardiez@fi.uba.ar>
2021-07-22 20:07:20 -0300

    Adjunte mi trabajo(mateo)

--- TP2.py
+++ TP2.py
@@ -2,7 +2,7 @@
 import os
 import csv
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 =======
 from utils import * 
 from gmailUtils import *
@@ -11,6 +11,13 @@ import io
 import zipfile
 DIRECTORIO_BASE = os.path.dirname(os.path.abspath(__file__))
 >>>>>>> 3b6b11407bc5a7910fad9c33d181cddadd2d3874
+
+from  __future__ import print_function
+from quickstart import obtener_servicio
+import shutil
+from googleapiclient.http import MediaIoBaseDownload
+import shutil
+from oauth2client import file, client, tools
 
 def ingresar_opcion():
     print('\n')
@@ -32,8 +39,148 @@ def ingresar_opcion():
     opcion = int(opcion)
     return opcion
 
+def crear_archivo():
+    '''
+    pre:
+    post: Crea un archivo vacio, segun la extension que elija el usuario
+    '''
+    mimetype = ''
+    tipo_de_archivo = [['1) .txt','text/plain'],['2) .jpeg','image/jpeg'],['3) .pdf','application/pdf'],['4) .zip','application/zip']]
+    for i in range(len(tipo_de_archivo)):
+        print(tipo_de_archivo[i][0])
+    tipo_archivo = int(input('Eleji el tipo de archivo que queres para tu nuevo archivo: '))
+    tipo_archivo-=1
+    mimetype = tipo_de_archivo[tipo_archivo][1]
+    print(mimetype)
+    nombre_archivo = input('Ingrese el nombre del nuevo archivo: ')
+    file_metadata = {
+        'name' : nombre_archivo,
+        'mimeType' : mimetype
+        }
+    file = obtener_servicio().files().create(body=file_metadata,
+                                fields='id').execute()
+    print('File ID: %s' % file.get('id'))
+
+def crear_carpeta():
+    '''
+    pre:
+    post: crea la cantidad de carpetas que quiera el usuario con sus nombres
+    '''
+    nombre_de_carpetas = input('Ingrese los nombres de las distintas carpetas que quiere crear, separadas con una coma: ')
+    lista_nombre_carpetas = nombre_de_carpetas.split(',')
+    for nombre in lista_nombre_carpetas:
+        file_metadata = {
+            'name': nombre,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+        obtener_servicio().files().create(body=file_metadata).execute()
+
+def recorrer_el_drive():
+    '''
+    pre:
+    post: Le muestra todos los archivos y carpetas que hay en el drive
+    '''
+    print('Listado de archivos en remoto: ')
+    nombre_archivos = list()
+    archivos = obtener_servicio().files().list().execute()
+    print('\n')
+    for archivo in archivos:
+        if archivo == 'files':
+            print('\n')
+            for i in range(len(archivos[archivo])):
+
+                if archivos[archivo][i]['mimeType'] == 'application/vnd.google-apps.folder':
+                    print(i+1,') Carpeta = ','Nombre: ',archivos[archivo][i]['name'],' - ',archivos[archivo][i]['mimeType'],' - id: ',archivos[archivo][i]['id'] )
+                    query = f"parents = '{archivos[archivo][i]['id']}'"
+                    archivos_de_carpeta = obtener_servicio().files().list(q=query).execute()
+                    for archivo_carpeta in archivos_de_carpeta:
+                        if archivo_carpeta == 'files':
+                            for x in range(len(archivos_de_carpeta[archivo_carpeta])):
+                                print(' --- ',i+1,'.',x+1,')Archivo_de_carpeta:','Nombre: ',archivos_de_carpeta[archivo_carpeta][x]['name'],' - ',archivos_de_carpeta[archivo_carpeta][x]['mimeType'],' - id: ',archivos_de_carpeta[archivo_carpeta][x]['id'])
+                                nombre_archivos.append(archivos_de_carpeta[archivo_carpeta][x]['name'])
+
+                elif archivos[archivo][i]['name'] not in nombre_archivos:
+                    print(i+1,') Archivo =','Nombre: ',archivos[archivo][i]['name'],' - ',archivos[archivo][i]['mimeType'],' - id: ',archivos[archivo][i]['id'])
+    print('\n')
+
+def limpiar_carpeta_archivos_a_subir():
+    '''
+    luego de que el usuario ingrese archivos a la carpeta "Archivos a subir", esta funcion vacia la carpeta para nuevo uso
+    '''
+    ruta_archivos_a_subir = 'Archivos_a_subir'
+    for archivo in os.listdir(ruta_archivos_a_subir):
+        archivo = os.path.join(ruta_archivos_a_subir, archivo)
+        try:
+            if os.path.isfile(archivo):
+                os.unlink(archivo)        
+        except Exception as e:
+            print(e)
+
+    print('Se limpio la carpeta')
+
+def subir_archivo():
+    '''
+    pre:
+    post: sube archivos al drive
+    '''
+    print('Debe llevar todos los archivos que quiera subir a la carpeta "Archivos_a_subir". ')
+    enter = input('Si ya lo hizo apreta enter. ')
+    print('\n')
+
+    ruta_archivos_a_subir = 'Archivos_a_subir'
+    archivos_a_subir = [nombre for nombre in os.listdir(ruta_archivos_a_subir) if isfile(join(ruta_archivos_a_subir,nombre))]#Devuelve los archivos de la carpeta
+
+    print(archivos_a_subir)
+    for filename in archivos_a_subir:
+        ruta_archivo= os.path.join(ruta_archivos_a_subir, filename)
+        metadata = {'name': filename}
+        mimeType = None #Pongo = none para que me tome el tipo de archivo como viene, y no me lo cambie a tipo google docs
+        if mimeType:
+            metadata['mimeType'] = mimeType
+        res = obtener_servicio().files().create(body=metadata, media_body=ruta_archivo).execute()
+        if res:
+            print('Se subio el archivo: ', filename, res['mimeType'])
+
+    limpiar_carpeta_archivos_a_subir()
+
+def descargar_archivo():
+    '''
+    pre: debe haber archivos en el drive
+    post: descarga el archivo que eliga el usuario del drive
+    '''
+    archivos = obtener_servicio().files().list().execute()#Primero recorro el drive para que el usuario elija cual archivo quiere descargar
+    for archivo in archivos:
+        if archivo == 'files':
+            print('\n')
+            for i in range(len(archivos[archivo])):
+                print(i+1,')  Nombre: ',archivos[archivo][i]['name'],' - ',archivos[archivo][i]['mimeType'],' - id: ',archivos[archivo][i]['id'] )
+    archivo_a_descargar = int(input('Ingrese el nro del archivo que quiere descargar: '))
+    archivo_a_descargar-=1
+    file_id = archivos[archivo][archivo_a_descargar]['id']
+    opcionx = input('Desea modificar el nombre del archivo que va a guardar?(s|n): ')
+    if opcionx == 's':
+        nombre_archivo =input('Ingrese el nombre con el que quiere guardar el archivo: ')
+    else:
+        nombre_archivo = archivos[archivo][archivo_a_descargar]['name']
+    request = obtener_servicio().files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+
+    fh.seek(0)
+
+    with open(os.path.join('./Archivos_descargados', nombre_archivo), 'wb') as f:# Te guarda los archivos que descargas en la carpeta 'Archivos descargados'
+        f.write(fh.read())
+        f.close()
+
 def listar_directorio(ruta):
+    print('Listado de carpetas en local: ')
     archivos = [a for a in listdir(ruta) if isfile(join(ruta, a))]
+    
+    print('\n')    
     return archivos
 
 def init():
@@ -75,7 +222,6 @@ def getEmailSubject(data : dict = {}) -> str:
 def getAttachmentsIds(parts : dict = {}):
     partsWithAttachments = list(filter(lambda item: 'attachmentId' in item['body'], parts))
     return list(map(lambda item: item['body']['attachmentId'] ,partsWithAttachments))
-
 
 def crear_carpetas():
     alumnos = []
@@ -124,20 +270,32 @@ def main():
         opcion = ingresar_opcion()
 
         if opcion == 1:
-            listar_directorio(ruta)
-            listado_archivos = listar_directorio(ruta)
-            print(listado_archivos)
-            print(len(listado_archivos))
-        
+            opcion2 = int(input('Si desea hacerlo en local apreta 1, si desea en remoto apreta 2: '))
+            if opcion2 == 1:
+                listar_directorio(ruta)
+                listado_archivos = listar_directorio(ruta)
+                print(listado_archivos)
+                print(len(listado_archivos))
+
+            if opcion2 == 2:
+                recorrer_el_drive()
+
         if opcion == 2:
-            init()
-            
+            opcion2 = int(input('Si desea hacerlo en local apreta 1, si desea en remoto apreta 2: '))
+            if opcion2 == 1:
+                init()
+            if opcion2 == 2:
+                opcion3 = int(input('Si desea crear un archivo ingresa 1, si desea crear una/s carpeta/s ingresa 2: '))
+                if opcion3 == 1:
+                    crear_archivo()
+                if opcion3 == 2:
+                    crear_carpeta()
 
         if opcion == 3:
-            pass
+            subir_archivo()
         
         if opcion == 4:
-            pass
+            descargar_archivo()
 
         if opcion == 5:
             pass
@@ -152,8 +310,6 @@ def main():
         
         if opcion == 8:
             corte = True
-
-    print('Hola')
 
 
 
