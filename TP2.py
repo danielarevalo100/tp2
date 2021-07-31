ad3862710e8d1db06966d5e139d267d38274f365
MateoLardiez <mlardiez@fi.uba.ar>
2021-07-30 23:50:01 -0300

    Arreglando cosas

--- TP2.py
+++ TP2.py
@@ -10,10 +10,10 @@ import zipfile
 from service_drive import obtener_servicio
 from service_gmail import obtener_servicio_gmail
 import shutil
-from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 import shutil
-#from oauth2client import file, client, tools
-
+import time
+from datetime import datetime, timedelta
 
 #Modulos
 from listar_archivos import listar_archivos
@@ -30,6 +30,7 @@ menu = (
     'Actualizar entregas de alumnos viıa mail.',
     'Salir.'
 )
+
 
 def crear_archivo():
     '''
@@ -67,33 +68,24 @@ def crear_carpeta():
         }
         obtener_servicio().files().create(body=file_metadata).execute()
 
-def recorrer_el_drive():
+def recorrer_el_drive(id='root', profundidad=0):
     '''
     pre:
     post: Le muestra todos los archivos y carpetas que hay en el drive
     '''
-    print('Listado de archivos en remoto: ')
-    nombre_archivos = list()
-    archivos = obtener_servicio().files().list().execute()
-    print('\n')
+    driveid = obtener_servicio().files().get(fileId=id).execute()['id']
+    query = f"parents = '{driveid}'"
+    archivos = obtener_servicio().files().list(q=query).execute()['files']
+
     for archivo in archivos:
-        if archivo == 'files':
-            print('\n')
-            for i in range(len(archivos[archivo])):
-
-                if archivos[archivo][i]['mimeType'] == 'application/vnd.google-apps.folder':
-                    print(i+1,') Carpeta = ','Nombre: ',archivos[archivo][i]['name'],' - ',archivos[archivo][i]['mimeType'],' - id: ',archivos[archivo][i]['id'] )
-                    query = f"parents = '{archivos[archivo][i]['id']}'"
-                    archivos_de_carpeta = obtener_servicio().files().list(q=query).execute()
-                    for archivo_carpeta in archivos_de_carpeta:
-                        if archivo_carpeta == 'files':
-                            for x in range(len(archivos_de_carpeta[archivo_carpeta])):
-                                print(' --- ',i+1,'.',x+1,')Archivo_de_carpeta:','Nombre: ',archivos_de_carpeta[archivo_carpeta][x]['name'],' - ',archivos_de_carpeta[archivo_carpeta][x]['mimeType'],' - id: ',archivos_de_carpeta[archivo_carpeta][x]['id'])
-                                nombre_archivos.append(archivos_de_carpeta[archivo_carpeta][x]['name'])
-
-                elif archivos[archivo][i]['name'] not in nombre_archivos:
-                    print(i+1,') Archivo =','Nombre: ',archivos[archivo][i]['name'],' - ',archivos[archivo][i]['mimeType'],' - id: ',archivos[archivo][i]['id'])
-    print('\n')
+        espacios = ''
+        for i in range(profundidad):
+            espacios += '   '
+        if archivo['mimeType'] == 'application/vnd.google-apps.folder':
+            print(espacios, 'carpeta', archivo['name'])
+            recorrer_el_drive(archivo['id'], profundidad+1)
+        else:
+            print(espacios, archivo['name'], 'id: ',archivo['id'])
 
 def limpiar_carpeta_archivos_a_subir():
     '''
@@ -135,41 +127,19 @@ def subir_archivo():
 
     limpiar_carpeta_archivos_a_subir()
 
-def descargar_archivo():
+def descargar_archivo(basedir):
     '''
     pre: debe haber archivos en el drive
     post: descarga el archivo que eliga el usuario del drive
     '''
-    nombre_archivos = list()
-    archivos = obtener_servicio().files().list().execute()
+    recorrer_el_drive()
     print('\n')
-    for archivo in archivos:
-        if archivo == 'files':
-            print('\n')
-            for i in range(len(archivos[archivo])):
-                if archivos[archivo][i]['mimeType'] == 'application/vnd.google-apps.folder':
-                    print(i+1,') Carpeta = ','Nombre: ',archivos[archivo][i]['name'],' - ',archivos[archivo][i]['mimeType'],' - id: ',archivos[archivo][i]['id'] )
-                    query = f"parents = '{archivos[archivo][i]['id']}'"
-                    archivos_de_carpeta = obtener_servicio().files().list(q=query).execute()
-                    for archivo_carpeta in archivos_de_carpeta:
-                        if archivo_carpeta == 'files':
-                            for x in range(len(archivos_de_carpeta[archivo_carpeta])):
-                                print(' --- ',i+1,'.',x+1,')Archivo_de_carpeta:','Nombre: ',archivos_de_carpeta[archivo_carpeta][x]['name'],' - ',archivos_de_carpeta[archivo_carpeta][x]['mimeType'],' - id: ',archivos_de_carpeta[archivo_carpeta][x]['id'])
-                                nombre_archivos.append(archivos_de_carpeta[archivo_carpeta][x]['name'])
-                elif archivos[archivo][i]['name'] not in nombre_archivos:
-                    print(i+1,') Archivo =','Nombre: ',archivos[archivo][i]['name'],' - ',archivos[archivo][i]['mimeType'],' - id: ',archivos[archivo][i]['id'])
-
-    id_correcto = False
-    while not id_correcto:#Analizo que el id sea de un archivo y no una carpeta
-        id_archivo = input('Copie y pegue el nro de id del archivo que desea descargar(no carpeta): ')
-        for i in range(len(archivos['files'])):
-            if archivos['files'][i]['id'] == id_archivo:
-                if archivos[archivo][i]['mimeType'] == 'application/vnd.google-apps.folder':
-                    print('El id que pego corresponde a una carpeta, copie el de un archivo')
-                    id_archivo = input('Copie y pegue el nro de id del archivo que desea descargar(no carpeta): ')
-                else:
-                    archivo_elejido = archivos['files'][i]
-                    id_correcto = True
+    archivos = obtener_servicio().files().list().execute()
+    id_archivo = input('Copie y pegue el nro de id del archivo que desea descargar: ')
+    for i in range(len(archivos['files'])):
+        if archivos['files'][i]['id'] == id_archivo:
+                archivo_elejido = archivos['files'][i]
+                
     opcionx = input('Desea modificar el nombre del archivo que va a guardar?(s|n): ')
     if opcionx == 's':
         nombre_archivo =input('Ingrese el nombre con el que quiere guardar el archivo: ')
@@ -179,15 +149,23 @@ def descargar_archivo():
     request = obtener_servicio().files().get_media(fileId=id_archivo)
     fh = io.BytesIO()
     downloader = MediaIoBaseDownload(fh, request)
+    opcion = int(input('Si desea guardar el archivo en una subcarpeta aprete 1, si desea que se guarde en la carpeta local aprete 2: '))
+    
     done = False
     while done is False:
         status, done = downloader.next_chunk()
         print("Download %d%%." % int(status.progress() * 100))
 
     fh.seek(0)
-    with open(os.path.join('./Archivos_descargados', nombre_archivo), 'wb') as f:# Te guarda los archivos que descargas en la carpeta 'Archivos descargados'
-        f.write(fh.read())
-        f.close()
+    
+    if opcion == 1:
+        with open(os.path.join('./Archivos_descargados', nombre_archivo), 'wb') as f:# Te guarda los archivos que descargas en la carpeta 'Archivos descargados'
+            f.write(fh.read())
+            f.close()
+    elif opcion ==2:
+        with open(os.path.join(basedir, nombre_archivo), 'wb') as f:# Te guarda en la carpeta local
+            f.write(fh.read())
+            f.close()
 
 def ultima_modificacion_local(ruta_archivo):
     '''
@@ -199,52 +177,83 @@ def ultima_modificacion_local(ruta_archivo):
     fecha = datetime(fecha[0], fecha[1], fecha[2], fecha[3], fecha[4], fecha[5])
     return fecha
 
-def sincronizacion(ruta):
+def diccionarios_archivos(basedir):
+    '''
+    genera diccionarios con los archivos/carpetas locales y remotos
+    '''
     archivos_remotos = obtener_servicio().files().list(fields='files(name,modifiedTime,id,mimeType)').execute()
-    archivos_locales = [a for a in listdir(ruta) if isfile(join(ruta, a))]
+    dict_archivos_remoto = dict()
+    for archivo_remoto in archivos_remotos['files']:
+        lista_fecha = archivo_remoto['modifiedTime'].split('.')
+        lista_fecha.pop(1)
+        ultima_modificacion_remoto = (''.join(lista_fecha))
+        desfase_horario = timedelta(hours = 3)
+        ultima_modificacion_remoto = datetime.strptime(ultima_modificacion_remoto, '%Y-%m-%dT%H:%M:%S') - desfase_horario
+        dict_archivos_remoto[archivo_remoto['name']] = [ultima_modificacion_remoto, archivo_remoto['id'], archivo_remoto['mimeType']]
+
+    with os.scandir(basedir) as ficheros:
+        archivos_locales = list()
+        for i in ficheros:
+            archivos_locales.append(i.name)
+    dict_archivos_locales = dict()
     for archivo_local in archivos_locales:
-        for archivo_remoto in archivos_remotos:
-            if archivo_remoto == 'files':
-                for i in range(len(archivos_remotos[archivo_remoto])):
-                    if archivo_local == archivos_remotos[archivo_remoto][i]['name']:
-                        lista_fecha = archivos_remotos[archivo_remoto][i]['modifiedTime'].split('.')
-                        lista_fecha.pop(1)
-                        ultima_modificacion_remoto = (''.join(lista_fecha))
-                        ultima_modificacion_remoto = datetime.strptime(ultima_modificacion_remoto, '%Y-%m-%dT%H:%M:%S')
-                        
-                        if ultima_modificacion_local(archivo_local) > ultima_modificacion_remoto:
-                            contenido_archivo = MediaFileUpload(archivo_local, mimetype=archivos_remotos[archivo_remoto][i]['mimeType'])
-                            obtener_servicio().files().update(fileId=archivos_remotos[archivo_remoto][i]['modifiedTime'], media_body=contenido_archivo)
-                            print('hola')
+        dict_archivos_locales[archivo_local] = ultima_modificacion_local(archivo_local)
 
-                        elif ultima_modificacion_local(archivo_local) < ultima_modificacion_remoto:
-                            print('chau')
-                            if (os.path.isfile(archivo_local)): # elimina el archivo
-                                os.remove(archivo_local)
+    return dict_archivos_locales, dict_archivos_remoto
 
-                                request = obtener_servicio().files().get_media(fileId=id_archivo)#Descarga archivo
-                                fh = io.BytesIO()
-                                downloader = MediaIoBaseDownload(fh, request)
-                                done = False
-                                while done is False:
-                                    status, done = downloader.next_chunk()
-                                    print("Download %d%%." % int(status.progress() * 100))
-                                fh.seek(0)
-                                with open(os.path.join('./Archivos_descargados', nombre_archivo), 'wb') as f:# Te guarda los archivos que descargas en la carpeta 'Archivos descargados'
-                                    f.write(fh.read())
-                                    f.close()
-                        elif ultima_modificacion_local(archivo_local) == archivos_remotos[archivo_remoto][i]['modifiedTime']:
-                            print('No se modificaron')
+def comparar_fechas(dict_archivos_locales,dict_archivos_remoto,archivo):
+    '''
+    pre: un archivo debe estar en la carpeta local como en la de remoto
+    post: compara las fechas de modificacion y remplaza el archivo mas viejo
+    '''
+    modificacion_local = dict_archivos_locales[archivo]
+    modificacion_remoto = dict_archivos_remoto[archivo][0]
+    print('mod local: ',modificacion_local,'archivo: ',archivo)
+    print('mod remoto: ',modificacion_remoto,'archivo: ',archivo)
 
-                    else:
-                        #Debe subir el archivo al remoto
-                        metadata = {'name': archivo_local} 
-                        mimeType = None #Pongo = none para que me tome el tipo de archivo como viene, y no me lo cambie a tipo google docs
-                        if mimeType:
-                            metadata['mimeType'] = mimeType
-                        res = obtener_servicio().files().create(body=metadata, media_body=archivo_local).execute()
-                        if res:
-                            print('Se subio el archivo: ', archivo_local, res['mimeType'])
+    if modificacion_local > modificacion_remoto:#Modifica el archivo que esta en el drive por el que esta en local
+        contenido_archivo = MediaFileUpload(archivo, mimetype= dict_archivos_remoto[archivo][2])
+        obtener_servicio().files().update(fileId=dict_archivos_remoto[archivo][1], media_body=contenido_archivo).execute()
+        print('Se modifico el archivo', archivo)
+
+    elif modificacion_local < modificacion_remoto:
+        if (os.path.isfile(archivo)): # elimina el archivo
+            os.remove(archivo)
+            request = obtener_servicio().files().get_media(fileId=dict_archivos_remoto[archivo][1])#Descarga archivo
+            fh = io.BytesIO()
+            downloader = MediaIoBaseDownload(fh, request)
+            fh.seek(0)
+            with open(os.path.join(basedir, archivo), 'wb') as f:# Te guarda los archivos que descargas en la carpeta 'Archivos descargados'
+                f.write(fh.read())
+                f.close()
+    
+    
+    elif modificacion_local == modificacion_remoto:
+        print('No se modifico el archivo ', archivo)
+
+def comparar_archivos(dict_archivos_locales, dict_archivos_remoto):
+    cosas_quenoquiero_subir = ['__pycache__','Archivos_a_subir','Archivos_descargados','.gitignore','actualizar_entregas.py','client_secret_drive.json','crear_carpetas.py','gmailUtils.py','listar_archivos.py','README.md','service_drive.py','service_gmail.py','TP2.py','utils.py','token_drive.json']#Por si hay archivos en la carpeta principal que no queres subir
+    list_remoto = list() #Se usa para ver los archivos que estan en el remoto pero no en el drive
+
+    for claves, valores in dict_archivos_locales.items():
+        print('Nombre archivo local:',claves, 'Fecha modificacion: ',valores)
+    print('\n')
+    for claves, valores in dict_archivos_remoto.items():
+        print('Nombre archivo remoto:',claves, 'Fecha modificacion: ',valores)
+    print('\n')
+    print('\t----')
+    print('\n')
+    for claves, valores in dict_archivos_locales.items():
+        if claves in dict_archivos_remoto:
+            #LLamo a la funcion que analiza las fechas de modificacion
+            comparar_fechas(dict_archivos_locales, dict_archivos_remoto,claves)
+        elif claves not in cosas_quenoquiero_subir:
+            #Debe subir el archivo al remoto
+            print('hola')
+            # metadata = {'name': claves}
+            # contenido = MediaFileUpload(claves)
+            # obtener_servicio().files().create(body=metadata,media_body=contenido).execute()
+            print('Se subio el archivo: ', claves)
 
 def crear_carpeta_local():
     print("ver archivos y carpetas")
@@ -288,7 +297,6 @@ def getAttachmentsIds(parts : dict = {}):
 
 def main():
     corte = False
-    #service = obtener_servicio()
     basedir = os.path.dirname(os.path.abspath(__file__))
   
     while not corte:
@@ -298,9 +306,6 @@ def main():
             opcion2 = int(input('Si desea hacerlo en local apreta 1, si desea en remoto apreta 2: '))
             if opcion2 == 1:
                listar_archivos() 
-
-                #print(listado_archivos)
-
             if opcion2 == 2:
                 recorrer_el_drive()
 
@@ -319,10 +324,11 @@ def main():
             subir_archivo()
         
         if opcion == 4:
-            descargar_archivo()
+            descargar_archivo(basedir)
         
         if opcion == 5:
-            sincronizacion(basedir)
+            dict_archivos_locales, dict_archivos_remoto = diccionarios_archivos(basedir)
+            comparar_archivos(dict_archivos_locales, dict_archivos_remoto)
 
         if opcion == 6:
             crear_carpetas(basedir)
@@ -332,6 +338,5 @@ def main():
 
         if opcion == 8:
             corte = True
-
 
 main()
